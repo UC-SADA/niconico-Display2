5d52d3f13579941ca3aa45f6091adbdb64073836
sadamitsu yushi <uc.sadamitsu@gmail.com>
2018-11-04 07:54:21 +0900

    read me

--- index.js
+++ index.js
@@ -15,29 +15,6 @@ const addresses = Object.keys(interfaces)
   .filter((iface) => iface.family === 'IPv4' && !iface.internal)
   .map((iface) => iface.address);
 
-
-function getLocalAddress() {
-    var ifacesObj = {}
-    ifacesObj.ipv4 = [];
-    ifacesObj.ipv6 = [];
-    var interfaces = os.networkInterfaces();
-
-    for (var dev in interfaces) {
-        interfaces[dev].forEach(function(details){
-            if (!details.internal){
-                switch(details.family){
-                    case "IPv4":
-                        ifacesObj.ipv4.push({name:dev, address:details.address})
-                    break;
-                    case "IPv6":
-                        ifacesObj.ipv6.push({name:dev, address:details.address})
-                    break;
-                }
-            }
-        });
-    }
-    return ifacesObj;
-};
 http.listen(2525, function(){
   console.log(addresses + ":2525");
 });
